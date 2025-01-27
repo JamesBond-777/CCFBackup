325d66dc096081b24fd6c008d46a0d4f6bd081cf
whitehat-007 <chinmaynavik2002@gmail.com>
2025-01-27 14:53:53 -0500

    chnages

--- php_views/final_owner/add_user.php
+++ php_views/final_owner/add_user.php
@@ -3,7 +3,7 @@
 $pageTitle = "O - Add User"; 
 
 // Include the header
-include('../asset_for_pages/admin_header.php');
+include('../asset_for_pages/owner_header.php');
 ?>
 
 <div class="page-header">
